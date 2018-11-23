f17f829b6bc3dd6f5ca62835efa500e4de59f04e
Elias <elias.el.khaldi@gmail.com>
2018-11-23 12:08:03 +0100

    added invert effect at route /b

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,26 @@ router.get('/', function(req, res) {
   return res.download('routes/smiley.png');
 });
 
+router.post('/b', function(req, res) {
+  var image = req.body;
+
+  try{
+    Jimp.read(image, (err, input) => {
+      if (err) throw err;
+      input.invert();
+
+      input.getBuffer(Jimp.AUTO, (err, output) => {
+        if(err) throw err;
+
+        res.writeHead(200, {'Content-Type': 'image/png' });
+        return res.end(output, 'binary');
+      });
+    });
+  }catch (err){
+    return res.status(400).send(`Error: ${err.message}`).end();
+  }
+});
+
 router.post('/a', function(req, res) {
   var image = req.body;
 

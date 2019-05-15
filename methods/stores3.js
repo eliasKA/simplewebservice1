19455f68ad5f8164d08bb3fd9164f30c2fcc965f
EC2 Default User <elias.el.khaldi@gmail.com>
2019-05-15 14:53:55 +0000

    added some stuff

--- methods/stores3.js
+++ methods/stores3.js
@@ -7,9 +7,9 @@ var exec = util.promisify(require('child_process').exec);
 
 module.exports.store = (image, name) => {
   //only works on linux
-  var tmp = '/simplewebservice1/public/tmp';
+  var tmp = './tmp';
   fs.createWriteStream(tmp).write(image);
   
-  return exec('aws s3 cp /simplewebservice1/public/tmp s3://ff-elias-test-bucket/test1/' + name);
+  return exec('aws s3 cp /dev/simplewebservice1/methods/tmp s3://ff-elias-test-bucket/test1/' + name);
 }
 
